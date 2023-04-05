ebd493e71183ff547c52bb2b0dc0455dc8596c51
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-04-05 14:59:10 -0700

    changes mockups

--- js/main.js
+++ js/main.js
@@ -37,7 +37,9 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    }
+    } else { 
+      entry.target.classList.remove("show")
+    };
   });
 });
 
