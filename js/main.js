5d039c35a53e3f76a074e4aed4e1c27f4a8f16d7
Meghan Bucher <115611931+bardIRL@users.noreply.github.com>
2023-04-06 11:01:49 -0700

    adds colorful tags to skills section, changes about me section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const nameEl = document.getElementById("meghan");
+const nameEl = document.querySelector(".main-heading");
 const strNameEl = nameEl.textContent;
 const splitNameEl = strNameEl.split("");
 const hiddenEls = document.querySelectorAll(".hidden");
-const navIcon = document.getElementById('nav-icon');
-const navMenu = document.getElementById('mobile-menu');
+const navIcon = document.getElementById("nav-icon");
+const navMenu = document.getElementById("mobile-menu");
 
 // animated text
 nameEl.textContent = "";
@@ -35,9 +35,9 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else { 
-      entry.target.classList.remove("show")
-    };
+    } else {
+      entry.target.classList.remove("show");
+    }
   });
 });
 
@@ -45,18 +45,19 @@ hiddenEls.forEach((el) => observer.observe(el));
 
 // mobile nav menu
 function toggleNavMenu() {
-  navIcon.classList.toggle('open');
-  if (navIcon.classList.contains('open')) {
-    navMenu.style.display = 'flex';
+  navIcon.classList.toggle("open");
+  if (navIcon.classList.contains("open")) {
+    console.log("wow");
+    navMenu.style.display = "flex";
   } else {
-    navMenu.style.display = 'none';
+    navMenu.style.display = "none";
   }
 }
 
 function closeNavMenu() {
-  navIcon.classList.remove('open');
+  navIcon.classList.remove("open");
   navMenu.style.display = "none";
 }
 
-navIcon.addEventListener("click", toggleNavMenu);
 window.addEventListener("resize", closeNavMenu);
+navIcon.addEventListener("click", toggleNavMenu);
